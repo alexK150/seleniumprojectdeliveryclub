b07fbc5bd7347e15567da46ab90b829933d435bc
Alex <akuzin20@gmail.com>
2020-03-16 18:21:55 +0300

    commit v1 refactor with annotation: Before and After

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,22 +1,33 @@
 package ru.deliveryClub;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
 
-    @Test
-    public void firstTest(){
+    public ChromeDriver driver;
+
+    @Before
+    public void init() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         //initialization of diver
-        ChromeDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        System.out.println("Test started");
+    }
 
+    @Test
+    public void firstTest() {
         driver.get("https://www.delivery-club.ru");
-
         String title = driver.getTitle();
         Assert.assertEquals("Доставка еды из ресторанов Москвы за 15-30 минут! Delivery Club", title);
+    }
 
+    @After
+    public void end() {
+        System.out.println("Test closed");
         driver.quit();
     }
 }
