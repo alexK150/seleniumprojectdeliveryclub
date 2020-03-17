5de5ef7942ed0a778b9941a3c729f2dcd0200db4
Alex <akuzin20@gmail.com>
2020-03-17 10:35:40 +0300

    commit v3 login click test added

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -2,6 +2,8 @@ package ru.deliveryClub;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class FirstTest extends WebdriverSettings {
 
@@ -10,5 +12,18 @@ public class FirstTest extends WebdriverSettings {
         driver.get("https://www.delivery-club.ru");
         String title = driver.getTitle();
         Assert.assertEquals("Доставка еды из ресторанов Москвы за 15-30 минут! Delivery Club", title);
+    }
+
+    @Test
+    public void loginTest(){
+        driver.get("https://www.delivery-club.ru");
+        //login click
+        WebElement loginBtn = driver.findElement(By.cssSelector(".header-login-button"));
+        loginBtn.click();
+
+        WebElement popUpWindow = driver.findElement(By.cssSelector(".popup--def__content"));
+        String expectedPopUpText = "Телефон для связи";
+        String actualPopUpText = popUpWindow.getText();
+        Assert.assertTrue(actualPopUpText.contains(expectedPopUpText));
     }
 }
